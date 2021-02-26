e49513d00434a195bf04f79961fba332a32fdc61
Ryan.K <ryankung@ieee.org>
2021-02-27 03:12:32 +0800

    test r1cs

--- ark/src/main.rs
+++ ark/src/main.rs
@@ -1,18 +1,20 @@
 use snark::r1cs::ConstraintSystem;
+use snark::r1cs::LinearCombination;
 use ec::models::bn::BnParameters;
 use r1cs::alloc::AllocationMode::{Input, Witness, Constant};
 use ff::fields::models::Fp256;
 use bn254::Fr;
+use std::ops::Mul;
 
 fn prove_sum() {
     let cs = ConstraintSystem::<Fr>::new_ref();
     let input = cs.new_input_variable(|| Ok(Fr::from(42u16))).unwrap();
-    // for value in values.iter() {
-    //     sum_v += var(&cs, value, Witness);
-    // }
-    // sum_i.enforce_equal(&sum_v).unwrap();
-    // cs.finalize();
-    // assert!(cs.is_satisfied().unwrap());
+    let mut lc = LinearCombination::<Fr>::zero();
+    lc = lc + input;
+    lc = lc.mul(Fr::from(3u16));
+    cs.new_lc(lc).unwrap();
+    cs.finalize();
+    println!("{:?}", cs);
 }
 
 fn main() {
