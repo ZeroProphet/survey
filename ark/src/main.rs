82e62a7d5aa30988c65ffba0a99187d6a7403d09
Ryan.K <ryankung@ieee.org>
2021-02-27 02:40:32 +0800

    test ark

--- ark/src/main.rs
+++ ark/src/main.rs
@@ -1,3 +1,20 @@
+use snark::r1cs::ConstraintSystem;
+use ec::models::bn::BnParameters;
+use r1cs::alloc::AllocationMode::{Input, Witness, Constant};
+use ff::fields::models::Fp256;
+use bn254::Fr;
+
+fn prove_sum() {
+    let cs = ConstraintSystem::<Fr>::new_ref();
+    let input = cs.new_input_variable(|| Ok(Fr::from(42u16))).unwrap();
+    // for value in values.iter() {
+    //     sum_v += var(&cs, value, Witness);
+    // }
+    // sum_i.enforce_equal(&sum_v).unwrap();
+    // cs.finalize();
+    // assert!(cs.is_satisfied().unwrap());
+}
+
 fn main() {
-    println!("Hello, world!");
+    prove_sum();
 }
