59a94578f90312c796a462952aab7e868d7155ac
Ryan.K <ryankung@ieee.org>
2021-03-01 19:32:35 +0800

    r1cs abc

--- ark/src/main.rs
+++ ark/src/main.rs
@@ -1,15 +1,23 @@
 use snark::r1cs::ConstraintSystem;
-use snark::r1cs::LinearCombination;
 use snark::r1cs::SynthesisMode;
+use snark::lc;
 use bn254::Fr;
 
-fn prove_sum() {
+fn prove_sum(input: Fr) {
     let cs = ConstraintSystem::<Fr>::new_ref();
+    let ret = input.clone() + input;
     cs.set_mode(SynthesisMode::Setup);
     // Obtain a variable representing a new private witness input.
-    let input = cs.new_input_variable(|| Ok(Fr::from(42u16))).unwrap();
-    let lc = LinearCombination::<Fr>::zero();
-    cs.new_lc(lc + input).unwrap();
+    let input_var = cs.new_input_variable(|| Ok(Fr::from(42u16))).unwrap();
+    let output_var = cs.new_witness_variable(|| Ok(ret)).unwrap();
+    cs.enforce_constraint(
+        lc!() + input_var,
+        lc!() + input_var,
+        lc!() + output_var
+    ).unwrap();
+
+    cs.new_lc(lc!() + input_var).unwrap();
+    cs.inline_all_lcs();
     cs.finalize();
     println!("{:?}", cs.clone());
     println!("================================");
@@ -18,5 +26,5 @@ fn prove_sum() {
 }
 
 fn main() {
-    prove_sum();
+    prove_sum(Fr::from(42u16));
 }
