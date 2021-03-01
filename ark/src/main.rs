6eb19dafa6c0dad6fd603b7eec77587471519640
Ryan.K <ryankung@ieee.org>
2021-03-01 19:48:05 +0800

    refine a + a

--- ark/src/main.rs
+++ ark/src/main.rs
@@ -1,3 +1,4 @@
+/// r1cs-std/target/doc/src/ark_relations/r1cs/constraint_system.rs.html#31-74
 use snark::r1cs::ConstraintSystem;
 use snark::r1cs::SynthesisMode;
 use snark::lc;
@@ -8,15 +9,16 @@ fn prove_sum(input: Fr) {
     let ret = input.clone() + input;
     cs.set_mode(SynthesisMode::Setup);
     // Obtain a variable representing a new private witness input.
-    let input_var = cs.new_input_variable(|| Ok(Fr::from(42u16))).unwrap();
-    let output_var = cs.new_witness_variable(|| Ok(ret)).unwrap();
+    let a = cs.new_input_variable(|| Ok(input)).unwrap();
+    let b = cs.new_input_variable(|| Ok(input)).unwrap();
+    let c = cs.new_witness_variable(|| Ok(ret)).unwrap();
     cs.enforce_constraint(
-        lc!() + input_var,
-        lc!() + input_var,
-        lc!() + output_var
+        lc!() + a,
+        lc!() + (ret, b),
+        lc!() + c
     ).unwrap();
 
-    cs.new_lc(lc!() + input_var).unwrap();
+    cs.new_lc(lc!() + a).unwrap();
     cs.inline_all_lcs();
     cs.finalize();
     println!("{:?}", cs.clone());
